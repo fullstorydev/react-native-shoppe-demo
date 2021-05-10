4dae5a29605a36cd6259aaacd5b9d46e32462fb2
sabrina-li <sabrinali0312@gmail.com>
2021-05-10 10:07:19 -0400

    Sabrina/market screen (#4)
    * components for market screen
    
    * market screen and example data
    
    * use local json when remote call fails
    
    * Remove console.log statements

--- src/screens/market.tsx
+++ src/screens/market.tsx
@@ -1,10 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { ScrollView } from 'react-native-gesture-handler';
+import { ActivityIndicator } from 'react-native-paper';
+import { ProductCard } from '../components';
+import { PRODUCT_URL } from '../../env.json';
+import { Product } from '../types';
+import products from '../data/products.json'
+import { AppDispatch } from '../redux/store';
+import RootActions from '../redux/actions';
 
 export const MarketScreen: React.FC = () => {
-  // TODO: implement the screen
+  const [isLoading, setLoading] = useState(true);
+  const [productList, setProductList] = useState<Product[]>([]);
+
+  const { addItem } = RootActions.ItemActions;
+  const dispatch: AppDispatch = useDispatch();
+
+  const addToCart = (product: Product) => { dispatch(addItem({...product, quantity:1}))} 
+  
+  // TODO:(sabrina) use dispatch to fetch data
+  useEffect(() => {
+    if (PRODUCT_URL === "") {
+      setProductList(products)
+    } else {
+      fetch(PRODUCT_URL)
+      .then((response) => response.json())
+      .then((json) => setProductList(json))
+      .catch((error) => {
+        console.error(error)
+        // just use the local json for demo purpose
+        setProductList(products)
+      })
+      .finally(() => setLoading(false));
+    }
+  }, []);
+
   return (
     <View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        {isLoading
+          ? <ActivityIndicator />
+          : productList.map(product => <View key={product.title}>
+            <ProductCard product={product} addToCart={addToCart} />
+          </View>
+          )}
+      </ScrollView>
     </View>
   );
 }
